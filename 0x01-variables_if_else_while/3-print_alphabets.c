4d328f573964d74b63444a6270b74b65a4589ffd
AdrianVides56 <adrianvj18@gmail.com>
2020-09-17 18:41:07 -0500

    Added 3-print_alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print the alpahbet using putchar
+ *
+ * Return: 0 (Success
+ */
+int main(void)
+{
+	char al, AL;
+	al = 'a';
+	AL = 'A';
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+		
+	}
+	while (AL <= 'Z')
+	{
+		putchar(AL);
+		AL++;
+		
+	}
+	putchar ('\n');
+	return (0);
+	
+}
