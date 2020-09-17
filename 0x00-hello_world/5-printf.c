2ffa6a8d5893c39ee7bdffa3d8e99d0bad8cc307
AdrianVides56 <adrianvj18@gmail.com>
2020-09-16 21:14:16 -0500

    Capitalizated R in comments_return

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
 * main - print a message with <printf> command
 *
-* return: 0 (Success)
+* Return: 0 (Success)
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
