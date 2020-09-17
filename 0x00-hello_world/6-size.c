6495459a8dfc21f21dded112007f1939ff36cc8b
AdrianVides56 <adrianvj18@gmail.com>
2020-09-17 10:07:29 -0500

    installed package libc6-dev-i386

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of char: %d\n", sizeof(char));
-printf("Size of int: %d\n", sizeof(int));
-printf("Size of float: %d\n", sizeof(float));
-printf("Size of long int: %d\n", sizeof(long int));
-printf("Size of long long int: %d\n", sizeof(long long int));
+printf("Size of a char: %ld\n", sizeof(char));
+printf("Size of a int: %ld\n", sizeof(int));
+printf("Size of a float: %ld\n", sizeof(float));
+printf("Size of a long int: %ld\n", sizeof(long int));
+printf("Size of a long long int: %ld\n", sizeof(long long int));
 return (0);
 }
