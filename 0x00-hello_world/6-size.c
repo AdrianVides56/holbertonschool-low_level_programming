604b2a89aee8766d5206ed515a165ca82d4b7b64
AdrianVides56 <adrianvj18@gmail.com>
2020-09-16 21:33:38 -0500

    Added long and lon long ints

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 printf("Size of char: %d\n", sizeof(char));
 printf("Size of int: %d\n", sizeof(int));
 printf("Size of float: %d\n", sizeof(float));
-printf("Size of double: %d\n", sizeof(double));
+printf("Size of long int: %d\n", sizeof(long int));
+printf("Size of long long int: %d\n", sizeof(long long int));
 return (0);
 }
