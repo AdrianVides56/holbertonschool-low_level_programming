7a043d7be7d314452113d34def1664cfc3b49477
AdrianVides56 <adrianvj18@gmail.com>
2020-09-16 20:31:51 -0500

    Added \n to print command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
 * main - print a message with <printf> command
 *
-* return: 0 (succes)
+* return: 0 (success)
 */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
-
