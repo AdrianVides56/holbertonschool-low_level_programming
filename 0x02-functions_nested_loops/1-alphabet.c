bc3f1ef790be216f231c87cb83e24c3d65a611bb
AdrianVides56 <adrianvj18@gmail.com>
2020-09-22 18:56:57 -0500

    Added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* main - print the alphaet in lowercase
+*
+* Return: 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+char al;
+al = 97;
+
+while (al <= 122)
+{
+_putchar(al);
+al++;
+}
+
+_putchar('\n');
+}
