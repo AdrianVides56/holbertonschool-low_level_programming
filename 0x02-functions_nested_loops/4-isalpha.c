46bc4cb1374cd7998ec6778bbe8068a44992eb72
AdrianVides56 <adrianvj18@gmail.com>
2020-09-23 11:38:57 -0500

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,21 @@
-h
+#include "holberton.h"
+
+/**
+* _isalpha - check for alphabetic character
+* @c: Is a character
+* Return: 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+if (c >= 0 && c <= 255)
+{
+return (1);
+	
+}
+else
+{
+return (0);	
+}
+
+}
