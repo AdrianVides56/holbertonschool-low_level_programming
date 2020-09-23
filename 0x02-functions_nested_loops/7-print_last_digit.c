96527b2412c25c1fc860f34d42c3926267f0cfe7
AdrianVides56 <adrianvj18@gmail.com>
2020-09-23 17:52:12 -0500

    Added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,19 @@
-h
+#include "holberton.h"
+
+/**
+* print_last_digit - print the the last digit of a number
+* @ld: number
+* Return: 0 (Success)
+*/
+
+int print_last_digit(int ld)
+{
+ld = ld % 10;
+
+if (ld < 0)
+{
+ld = ld * -1;
+}
+_putchar(ld + 48);
+return (ld);
+}
