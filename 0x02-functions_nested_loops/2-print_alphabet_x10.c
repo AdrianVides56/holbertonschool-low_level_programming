fc28ad77d8f27e3c5a548c0cabca58c4dc177e1c
AdrianVides56 <adrianvj18@gmail.com>
2020-09-22 20:02:52 -0500

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 - print the alphaet in lowercase ten times in different lines
+*
+* Return: 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+char al;
+int nl;
+
+for (nl = 0; nl <= 9; nl++)
+{
+for (al = 97; al <= 122; al++)
+{
+_putchar (al);
+}
+_putchar ('\n');
+
+}
+}
