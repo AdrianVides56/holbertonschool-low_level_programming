651b1a740fd2de670ccc3cbb0734ecbe7bc93a40
AdrianVides56 <adrianvj18@gmail.com>
2020-09-24 12:04:46 -0500

    Added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* mul - check for uppercase character
+* @a: Is a integer
+* @b: Is a integer
+* Return: 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
