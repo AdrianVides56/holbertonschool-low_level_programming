1e9b7a17a8d7fcc77f77e3ac3065253b6971e470
AdrianVides56 <adrianvj18@gmail.com>
2020-09-25 09:52:11 -0500

    Added code

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-h
+#include "holberton.h"
+
+#include "holberton.h"
+
+/**
+ * int - sum two integers
+ * @a: Is a integer
+ * @b: Is a integer
+ * Return: 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
