2d6991258446d252184688831ac10902f219c8b6
AdrianVides56 <adrianvj18@gmail.com>
2020-09-24 19:55:22 -0500

    Fixed betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,25 @@
 
 /**
  * print_line - Draws a straight line
- *
+ * @n: n is the number of times the character _ should be printed
  * Return: 0 (Success)
  */
 
 void print_line(int n)
 {
 	int a;
-	
-	
+
 	if (n <= 0)
 	{
 		_putchar ('\n');
 	}
+
 	else
 	{
-		for (a = 0; a < n; a++ )
-	_putchar ('_');
-	_putchar ('\n');
+		for (a = 0; a < n; a++)
+		{
+		_putchar ('_');
+		}
+		_putchar ('\n');
 	}
 }
