8ad8406b044f0d0bff4611117828202e9f5f0fa9
AdrianVides56 <adrianvj18@gmail.com>
2020-09-25 14:45:07 -0500

    Added code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,41 @@
-h
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98 ten times
+ * @n: n is the starting point number
+ * Return: 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+while (n > 98)
+{
+	printf("%d", n);
+	putchar(',');
+	putchar(' ');
+	n--;
+	if (n == 98)
+	{
+		printf("98");
+		break;
+	}
+}
+while (n < 98)
+{
+	printf("%d", n);
+	putchar(',');
+	putchar(' ');
+	n++;
+	if (n == 98)
+	{
+		printf("98");
+		break;
+	}
+}
+if (n == 98)
+{
+	putchar('\n');
+}
+putchar('\n');
+}
