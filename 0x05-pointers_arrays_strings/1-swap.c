af626f4a77cf7c53739c6e0bf6252f5379888c12
AdrianVides56 <adrianvj18@gmail.com>
2020-09-28 16:30:30 -0500

    Added new variable

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap the value of two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = 42;
+	*b = c;
+}
