8fa1269728aee358a703ad009ba1c2cfdb8d4fca
AdrianVides56 <adrianvj18@gmail.com>
2020-09-28 17:05:39 -0500

    Added function prototype of strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: Is a character
+ * Return: 0 (Succes)
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(s);
+	return (len);
+}
