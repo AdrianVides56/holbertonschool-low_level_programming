7d907109d8e0d8efb3fc6f66850b264d84fe3a88
AdrianVides56 <adrianvj18@gmail.com>
2020-10-01 19:10:32 -0500

    Added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: -15, 15 or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 < s2)
+	{
+		return (-15);
+	}
+	else if (s1 > s2)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
+}
