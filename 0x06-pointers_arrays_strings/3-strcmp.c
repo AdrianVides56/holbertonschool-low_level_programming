9445a5dda2ac875abb65cf537c79be0a2abccc88
AdrianVides56 <adrianvj18@gmail.com>
2020-10-01 19:32:22 -0500

    Added counter variable

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
+	int len1, len2;
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
+	if (len1 < len2)
 	{
 		return (-15);
 	}
-	else if (s1 > s2)
+	else if (len1 > len2)
 	{
 		return (15);
 	}
