b893b163b7684d4391dc6f718b05c20ff4cfb32e
AdrianVides56 <adrianvj18@gmail.com>
2020-10-10 20:37:56 -0500

    Added prototype

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the its name
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf ("%s\n", argv[0]);
+
+	if (argc)
+	{}
+	return (0);
+}
