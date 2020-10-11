474ede00d1783c7fbf81546d25deb6bb2619d6db
AdrianVides56 <adrianvj18@gmail.com>
2020-10-10 20:40:31 -0500

    Fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
 
 /**
  * main - program that prints the its name
+ * @argc: count argument
+ * @argv: vector argument
  * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	if (argc)
 	{}
