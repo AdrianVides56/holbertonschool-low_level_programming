343d95a1851deb1d865cfa746d8a947c6d9e3819
AdrianVides56 <adrianvj18@gmail.com>
2020-10-13 17:54:32 -0500

    Added data type to malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	a = malloc(sizeof(unsigned int) * size);
+	a = (char *)malloc(sizeof(unsigned int) * size);
 	if (a == NULL)
 		return (NULL);
 	for (d = 0; d != size; d++)
