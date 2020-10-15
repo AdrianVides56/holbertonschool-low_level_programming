5ff0488e688e6fc0bb8e7448008dc803802ec968
AdrianVides56 <adrianvj18@gmail.com>
2020-10-15 18:38:27 -0500

    Changed malloc statement

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,8 +2,8 @@
 
 /**
  * *array_range - creates an array of integers
- * @min: value of the array
- * @max: value of the array
+ * @min: minimim value of the array
+ * @max: maximum value of the array
  * Return: pointer to the array
  */
 
@@ -14,14 +14,14 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = (int *)malloc(sizeof(int) * (max + 1));
+	arr = (int *)malloc(sizeof(int) * (max - min + 1));
+
 	if (arr == NULL)
 		return (NULL);
 
 	for (a = 0; min <= max; a++)
 	{
-		arr[a] = min;
-		min++;
+		arr[a] = min++;
 	}
 	return (arr);
 }
