d418c8228eb3a756b311f97d75cc651df978ce96
AdrianVides56 <adrianvj18@gmail.com>
2020-10-21 14:48:47 -0500

    Changed data type

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,16 +5,14 @@
  *                   on each element of an array.
  * @array: array
  * @size: size of the array
- * @action: pointer to other function
+ * @action: pointer to other function to execute
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int count;
+	size_t count;
 
 	if (array == NULL || action == NULL)
-		return;
-	if (size <= 0)
 		return;
 
 	for (count = 0; array[count] < array[size]; count++)
