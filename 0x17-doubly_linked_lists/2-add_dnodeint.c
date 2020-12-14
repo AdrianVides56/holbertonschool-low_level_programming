e4b6a0eda64696b968aaec09cc0753dd68f1aca3
AdrianVides56 <adrianvj18@gmail.com>
2020-12-14 14:34:00 +0000

    Added conditional if *head != NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,9 +10,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -20,6 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+
 	*head = new;
 
 	return (new);
