c8c111979074724dcf46cae696444753b3f5b1c0
AdrianVides56 <adrianvj18@gmail.com>
2021-02-19 21:24:01 +0000

    changed place of additions and substractions in conditionals

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,20 +3,20 @@
 
 
 def island_perimeter(grid):
-    """ Method to calc the perimeter """
+    """ return the perimeter of the island """
     i, j, per = 0, 0, 0
     while i < len(grid):
         j = 0
         while j < len(grid[0]):
             if grid[i][j] == 1:
                 per += 4
-                if i - 1 > 0 and grid[i - 1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     per -= 1
-                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                if i < (len(grid[0]) - 1) and grid[i + 1][j] == 1:
                     per -= 1
-                if j - 1 > 0 and grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     per -= 1
-                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
+                if j < (len(grid[0]) - 1) and grid[i][j + 1] == 1:
                     per -= 1
             j += 1
         i += 1
