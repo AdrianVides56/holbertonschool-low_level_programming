ace34866fae2440a64451a5a0d0c73516c51b5c5
AdrianVides56 <adrianvj18@gmail.com>
2021-02-19 21:10:17 +0000

    Added task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module to calc the perimeter """
+
+
+def island_perimeter(grid):
+    """ Method to calc the perimeter """
+    i, j, per = 0, 0, 0
+    while i < len(grid):
+        j = 0
+        while j < len(grid[0]):
+            if grid[i][j] == 0:
+                j += 1
+                continue
+            else:
+                per += 4
+                if i - 1 > 0 and grid[i - 1][j] == 1:
+                    per -= 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                    per -= 1
+                if j - 1 > 0 and grid[i][j - 1] == 1:
+                    per -= 1
+                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
+                    per -= 1
+            j += 1
+        i += 1
+    return per
