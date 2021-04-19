74cba6bbf93522e8b4ff3c8bf91f10a1d137e41e
AdrianVides56 <adrianvj18@gmail.com>
2021-04-19 09:46:49 -0500

    Added main modules

---
+++ 0x04-more_functions_nested_loops/main_modules/3-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void);
+int main(void)
+{
+	print_numbers();
+	return (0);
+	
+}
---
+++ 0x04-more_functions_nested_loops/main_modules/4-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void);
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+	
+}
