3cc30c8f2c38c76c168c7153cd3c8952e976f400
AdrianVides56 <adrianvj18@gmail.com>
2021-04-20 15:45:11 -0500

    Added conditional if value is <= 0

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,9 +13,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = 0, aux = 0, idx = 0;
 
-	if (!array)
+	if (!array || value < 0)
 		return (-1);
-	if (value <= 0)
+	if (value == 0)
 		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
 	while (array[jump] < value && idx != 1)
 	{
