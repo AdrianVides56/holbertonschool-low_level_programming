3ef227c561ac655879301264e4baebb218a44df3
AdrianVides56 <adrianvj18@gmail.com>
2021-04-20 15:46:17 -0500

    revert two previous commits

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,10 +13,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = 0, aux = 0, idx = 0;
 
-	if (!array || value < 0)
+	if (!array)
 		return (-1);
-	if (value == 0)
-		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+
 	while (array[jump] < value && idx != 1)
 	{
 		if (jump >= size - 1)
